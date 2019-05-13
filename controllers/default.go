0ff5da5ca1f10b98b734eb814f0047a3798e1075
wuhao8 <wuhao8@wuhao8deMacBook-Pro.local>
2019-05-13 10:48:21 +0800

    add mysql conf

--- controllers/default.go
+++ controllers/default.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"github.com/astaxie/beego"
+	"youdidi/models"
 )
 
 type MainController struct {
@@ -9,6 +10,12 @@ type MainController struct {
 }
 
 func (c *MainController) Get() {
+	var list []*models.User
+	var dbUser models.User
+
+	dbUser.Query().All(&list)
+
+	c.Data["list"] = list
 	c.Data["Website"] = "beego.me"
 	c.Data["Email"] = "astaxie@gmail.com"
 	c.TplName = "index.html"
